7cdd98fc512b27aff6eea7874b3336aff9da5397
GameandDario <47669171+GameandDario@users.noreply.github.com>
2020-12-18 11:43:45 +0100

    add citationClass.js

---
+++ citationClass.js
@@ -0,0 +1,21 @@
+class Citation {
+    constructor(blockContext, blockSubject, blockVerb, blockObject) {
+        this.blockContext = blockContext;
+        this.blockSubject = blockSubject;
+        this.blockVerb = blockVerb;
+        this.blockObject = blockObject;
+    }
+}
+const subject = [{
+    content: '',
+    isPluriel: false
+}]
+
+const verb = [{
+    content: '',
+    isPluriel: false
+}]
+
+const object = [{
+    content: '',
+}]
